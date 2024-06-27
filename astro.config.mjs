c892ae7abc4508dcf111cc45624dd0c8b4f7c750
chenpingxin <chenpingxin@makeblock.com>
2024-06-27 18:01:04 +0800

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,33 @@
 import { defineConfig } from 'astro/config';
+import nodePolyfills from 'rollup-plugin-node-polyfills';
 
+import astroI18next from "astro-i18next";
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  integrations: [astroI18next()],
   adapter: cloudflare({
+    mode: 'directory',
     platformProxy: {
       enabled: true
     }
-  })
+  }),
+  vite: {
+    // plugins: [
+    //   nodePolyfills({
+    //     process: false
+    //   })
+    // ],
+    ssr: {
+      external: [
+        // "i18next",
+        // "i18next-browser-languagedetector",
+        // "i18next-http-backend",
+        // "i18next-fs-backend",
+        // "astro-i18next"
+      ]
+    }
+  }
 });
