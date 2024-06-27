b27e9297f3e73c5884918854686e190262e6f5f0
chenpingxin <chenpingxin@makeblock.com>
2024-06-27 18:37:28 +0800

    node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import astroI18next from "astro-i18next";
 
-// https://astro.build/config
-export default defineConfig({});
+import cloudflare from '@astrojs/cloudflare';
+
+export default defineConfig({
+  output: 'server',
+  integrations: [astroI18next()],
+  adapter: cloudflare(),
+});
