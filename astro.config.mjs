71744b0ab83378d0631a077ff276aa04b87bfd38
chenpingxin <chenpingxin@makeblock.com>
2024-06-28 09:18:38 +0800

    注释国际化使用

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
-import astroI18next from "astro-i18next";
+// import astroI18next from "astro-i18next";
 
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   output: 'server',
-  integrations: [astroI18next()],
+  // integrations: [astroI18next()],
   adapter: cloudflare(),
 });
